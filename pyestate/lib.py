1484d5ec2cb31da4c9189baa07bcc818362147a7
Andrew Barisser <barisser@protonmail.com>
2023-05-16 21:11:49 -0500

    wip

--- pyestate/lib.py
+++ pyestate/lib.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
 
 def fixed_rate_amortization(principal, interest, n_payments, payments_per_year=12):
 	"""
@@ -28,9 +29,9 @@ def fixed_rate_amortization(principal, interest, n_payments, payments_per_year=1
 	for n in range(n_payments):
 		interest_payment = princ * per_interval_interest
 		principal_payoff = total - interest_payment
-		data.append([n, interest_payment, principal_payoff, princ])
+		data.append([n, interest_payment, principal_payoff, princ, total])
 		princ -= principal_payoff
-	return pd.DataFrame(data, columns=['payment_n', 'interest_payment', 'principal_payment', 'principal_remaining'])
+	return pd.DataFrame(data, columns=['payment_n', 'interest', 'principal_payment', 'principal_remaining', 'total'])
 
 
 def payment_schedule(asset_price, LTV, interest, n_payments, property_tax_rate,
@@ -123,6 +124,121 @@ def note_yield(price, unpaid, rate, payments):
 	effective_rate = mid
 	return effective_rate
 
+def note_price(unpaid, rate, market_rate, payments):
+	"""
+	Imagine a mortgage with an unpaid balance, rate, and number of remaining payments.
+	What is this 'worth' under a new market rate?
+
+	principal * i*(1+i)^N / ((1+i)^N - 1) = principal2 * i2 * (1+i2)^N / ((1+i2)^N-1)
+	principal2 = principal * (i*(1+i)^N / ((1+i)^N - 1)) / (i2 * (1+i2)^N / ((1+i2)^N-1))
+
+	Basically if I created a new mortgage note today at the prevailing rates with the
+	exact same payment schedule, what would the balance be?
+
+	note yield and note price have a parity with one another
+	pyestate.note_price(1e6, 0.05, 0.04,60) = 1.023e6
+	pyestate.note_yield(1.023e6, 1e6, 0.05, 60) == 0.04
+
+	Basically a 5% note for 1e6 in a 4% yield regime would go for 1.023e6, and correspondingly
+	the yield for a 1.023e6-price and 1e6 unpaid balance note with 5% coupon is 4%.
+	"""
+	def _func(i, n):
+			i2 = (1.+i)**(1/12.)-1.
+			return i2*(1.+i2)**n / ((1.+i2)**n - 1.)
+	return unpaid * _func(rate, payments) / _func(market_rate, payments)
+
+def iterate_sim_once(rent_lambda, price_lambda, rate_lambda, cash, last_price, debt, last_rate, last_rent):
+	"""
+	dat is [next price, next rent, next rate]
+	"""
+	next_rate = rate_lambda(last_rate)
+	next_rent = rent_lambda(last_rent)
+	next_price = price_lambda(last_price, next_rent, next_rate)
+	dat = [next_price, next_rent, next_rate]
+
+	return dat
+
+def simulate_property(price, ltv, rate, payments, rent, rent_lambda, price_lambda, rate_lambda, cash,
+	vacancy_rate, property_tax_rate):
+	"""
+	"""
+	debt = ltv * price
+	asset_price = price
+	current_rate = rate
+	data = []
+
+	schedule = payment_schedule(asset_price, ltv, rate, payments, 0, 0., payments_per_year=12)
+
+	for t in range(payments):
+		asset_price, rent, rate = iterate_sim_once(rent_lambda, price_lambda, rate_lambda, cash, asset_price, debt, rate, rent)
+		events = []
+		schedule_row = schedule.iloc[t]
+		payments = schedule_row.total_payment # todo add property tax back in
+		payments += asset_price * property_tax_rate / 12.
+		cashflow = - payments
+
+		vacant = np.random.rand() < vacancy_rate
+		if not vacant:
+			cashflow += rent
+		else:
+			events.append('vacant')
+
+		cash += cashflow
+		if cash > 0:
+			cash *= (1. + max(rate - 0.03, 0.))**(1/12.) # safe treasuries
+		if cash < 0:
+			events.append('bankrupt')
+
+		debt -= schedule_row.principal_payment
+		events_str = ",".join(events)
+
+		dat = [asset_price, rent, rate, cash, debt, events_str, payments]
+		data.append(dat)
+
+	df = pd.DataFrame(data, columns=['price', 'rent', 'rate', 'cash', 'debt', 'events', 'payments'])
+	df['equity']= df['price'] - df['debt']
+	df['net_worth'] = df['equity'] + df['cash']
+
+	return df
+
+def simulate_many(n, price, ltv, rate, payments, rent, rent_lambda, price_lambda, rate_lambda, cash,
+	vacancy_rate, property_tax_rate):
+	agg_data = []
+	dfs = []
+	for i in range(n):
+		print("Simulating: {}".format(i))
+		tdf = simulate_property(price, ltv, rate, payments, rent, rent_lambda, price_lambda, rate_lambda, cash, vacancy_rate, property_tax_rate)
+		tdf['id'] = i
+		dfs.append(tdf)
+
+		bankrupt = len(tdf) < payments
+		IRR = (tdf.iloc[60].net_worth / tdf.iloc[0].net_worth) ** (12. / len(tdf)) - 1.
+		agg = [i, bankrupt, IRR]
+		agg_data.append(agg)
+
+	detailed = pd.concat(dfs)
+	aggs = pd.DataFrame(agg_data, columns=['id', 'bankrupt', 'irr'])
+	return aggs, detailed
+
+def rent_slow_increase(rent):
+	return rent * 1.08**(1./12.)
+
+def price_walk(price, rent, rate):
+	spread = 0.02
+	eff_rate = max(rate + 0.03, 0.04)
+	price = rent * 12. * (1. / eff_rate)
+	return price
+
+def rate_walk(last_rate):
+	max_rates = 0.045
+	min_rates = -0.005
+	delta = np.random.logistic(scale=0.0025)
+	rate = last_rate + delta
+	rate = min(max_rates, rate)
+	rate = max(min_rates, rate)
+
+	return rate
 
 
-
+#df1, df2 = simulate_many(30, 3e5, 0.75, 0.065, 360, 1800, rent_slow_increase, price_walk, rate_walk, 1e4, 0.01, 0.02)
+import pdb;pdb.set_trace()
